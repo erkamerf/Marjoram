8be24649373f911a83bd27ce0abaf18ce6931d49
erkamerf <erkamerf@yahoo.com>
2025-07-01 22:30:56 -0500

    edits to send_commands

--- Marjoram-Globals.lua
+++ Marjoram-Globals.lua
@@ -43,6 +43,7 @@ state.NotifyBuffs	  		= M(false, 'Notify Buffs') 	 --Set this to true if you wan
 
 --send_command('bind !@^f7 gs c toggle AutoWSMode') --Turns auto-ws mode on and off.
 --send_command('bind !^f7 gs c toggle AutoFoodMode') --Turns auto-ws mode on and off.
+send_command('bind f7 input /console send @all /attack <bt>')
 send_command('bind f9 gs c cycle Weapons') --Cycle through weapons sets.
 --send_command('bind @f8 gs c toggle AutoNukeMode') --Turns auto-nuke mode on and off.
 --send_command('bind ^f8 gs c toggle AutoStunMode') --Turns auto-stun mode off and on.
@@ -51,17 +52,17 @@ send_command('bind f9 gs c cycle Weapons') --Cycle through weapons sets.
 send_command('bind @pause gs c cycle AutoBuffMode') --Automatically keeps certain buffs up, job-dependant.
 send_command('bind @scrolllock gs c cycle Passive') --Changes offense settings such as accuracy.
 send_command('bind !f9 gs c cycle OffenseMode') --Changes offense settings such as accuracy.
-send_command('bind ^f9 gs c cycle HybridMode') --Changes defense settings for melee such as PDT.
+send_command('bind @f9 gs c cycle HybridMode') --Changes defense settings for melee such as PDT.
+send_command('bind ~f9 gs c cycle WeaponskillMode') --Allows automatic weaponskilling if the job is setup to handle it.
 send_command('bind f10 gs c cycle RangedMode') --Changes ranged offense settings such as accuracy.
-send_command('bind !f10 gs c cycle WeaponskillMode') --Allows automatic weaponskilling if the job is setup to handle it.
-send_command('bind ^f10 gs c set DefenseMode Physical') --Turns your physical defense set on.
+send_command('bind !f10 gs c set DefenseMode Physical') --Turns your physical defense set on.
 send_command('bind ~f10 gs c cycle PhysicalDefenseMode') --Changes your physical defense set.
 --send_command('bind !f10 gs c toggle Kiting') --Keeps your kiting gear on..
+send_command('bind @f10 gs c cycle CastingMode') --Changes your castingmode options such as magic accuracy.
 send_command('bind f11 gs c set DefenseMode Magical') --Turns your magical defense set on.
 send_command('bind ^f11 gs c cycle MagicalDefenseMode') --Changes your magical defense set.
-send_command('bind @f10 gs c cycle CastingMode') --Changes your castingmode options such as magic accuracy.
 send_command('bind !f11 gs c cycle ExtraMeleeMode') --Adds another set layered on top of your engaged set.
-send_command('bind ^f12 gs c cycle ResistDefenseMode') --Changes your resist defense set.
+send_command('bind @f12 gs c cycle ResistDefenseMode') --Changes your resist defense set.
 --send_command('bind f12 gs c set DefenseMode Resist') --Turns your resist defense set on.
 send_command('bind f12 gs c cycle IdleMode') --Changes your idle mode options such as refresh.
 send_command('bind !f12 gs c reset DefenseMode') --Turns your defensive mode off.
